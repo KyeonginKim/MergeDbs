44330bde2c668c2bb9a9c159d9a9c7040c2bf070
KyeonginKim <kkyeongin@gmail.com>
2019-02-07 15:04:19 +0900

    ini file을 통해 config 설정 할 수 있도록 추가

--- src/connection.py
+++ src/connection.py
@@ -7,30 +7,33 @@ class Connection:
     def __init__(self, url):
         self.connection = connection = sqlite3.connect(url)
         if connection is None:
-            print("database connection fail : [ url : {0} ]".format(url))
+            print("Database Connection Fail : [ URL : {0} ]".format(url))
             self.close()
 
     def close(self):
         self.connection.close()
 
     def attach_database(self, contacts):
-        query = 'attach \'{0}\' as {1};'.format(contacts, var)
+        query = 'ATTACH \'{0}\' AS {1};'.format(contacts, var)
         self.execute_query(query)
 
     def detach_database(self):
-        query = 'detach {0}'.format(var)
+        query = 'DETACH {0};'.format(var)
         self.execute_query(query)
 
-    def merge_table(self, table):
-        query = 'insert into {0} select * from {1};'.format(table, var + '.' + table)
+    def merge_table(self, table, pk):
+        query = 'INSERT INFO {0} SELECT * FROM {1} WHERE {2} NOT IN (SELECT {2} FROM main.{0});'\
+            .format(table, var + '.' + table, pk)
         self.execute_query(query)
 
     def execute_query(self, query):
         cursor = self.connection.cursor()
         try:
-            print("query : ", query)
             cursor.execute(query)
             result = cursor.fetchall()
+        except Exception:
+            print("Query with Error : ", query)
+            raise
         finally:
             cursor.close()
 
--- src/merge.py
+++ src/merge.py
@@ -2,7 +2,7 @@ from src.config import *
 from src.util import *
 from src.db_util import *
 from src.connection import Connection
-import sys
+
 
 db_path_1 = ''
 db_path_2 = ''
@@ -13,13 +13,15 @@ def print_diff_content(conf_list, content):
         table = conf['table']
         pk = conf['primary_key']
         if pk != 'NULL':
-            print('========[ {0} {1} Table Diff ]========'.format(table, content))
-            print(diff(select_pk_from_table(db_path_1, table, pk), select_pk_from_table(db_path_2, table, pk)))
+            print('[ {0} {1} Table Diff ]'.format(table, content))
+            print("--->", diff(select_pk_from_table(db_path_1, table, pk), select_pk_from_table(db_path_2, table, pk)))
 
 
 def print_diff(target_dbs, relation_dbs):
+    print('=================================')
     print_diff_content(target_dbs, "Target")
     print_diff_content(relation_dbs, "Relation")
+    print('=================================')
 
 
 def merge(target_dbs, relation_dbs):
@@ -27,10 +29,10 @@ def merge(target_dbs, relation_dbs):
     connection.attach_database(db_path_2)
 
     for conf in target_dbs:
-        connection.merge_table(conf['table'])
+        connection.merge_table(conf['table'], conf['primary_key'])
 
     for conf in relation_dbs:
-        connection.merge_table(conf['table'])
+        connection.merge_table(conf['table'], conf['primary_key'])
 
     connection.detach_database()
 
@@ -41,11 +43,9 @@ if __name__ == "__main__":
         db_path_1, db_path_2 = arg_parser()
         print_diff(config.merge_tables, config.relation_tables)
         merge(config.merge_tables, config.relation_tables)
-    except:
+    except RuntimeError as ex:
         # ===============
         # Error handle
         # ===============
-        print("Runtime Error :")
-        print("Error Type : ", sys.exc_info()[0])
-        print("Unexpected error : ", sys.exc_traceback)
+        print("Unexpected error : ", ex.__traceback__)
         raise
